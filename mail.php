95d2e5a017d7aca33f435f164c44c0f85aaca9af
Tortoise100101 <tor.two10101@gmail.com>
2019-02-19 18:10:21 +0900

    fsfa

--- mail.php
+++ mail.php
@@ -3,18 +3,17 @@
 	$username='common10111@gmail.com';
 	$password='Tmcitonetwothree';
 	$response=imap_open($imap,$username,$password);
-	if(!$response)
+	if(!$response){
 		echo "Failed\n";
-	else{
-		$message_ids=imap_search($response,'From "no-reply@spacee.jp"');
-		print_r($message_ids);
-		foreach($message_ids as $message_id){
-			$header=imap_headerinfo($response,$message_id);
-			$subject=mb_decode_mimeheader($header->subject);
-			echo $subject."\n";
-			$body=imap_fetchbody($response,$message_id,1,FT_INTERNAL);
-			$body=base64_decode($body);
-			echo $body;
-		}
+		exit(1);
+	}
+	$message_ids=imap_search($response,'From "no-reply"');
+	foreach($message_ids as $message_id){
+		$header=imap_headerinfo($response,$message_id);
+		$subject=mb_decode_mimeheader($header->subject);
+		echo $subject."\n";
+		$body=imap_fetchbody($response,$message_id,1,FT_INTERNAL);
+		$body=base64_decode($body);
+		echo $body;
 	}
 ?>
