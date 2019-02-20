bcb898e4b54d2f1baef72fbfb4aa0c2610e09498
Tortoise100101 <tor.two10101@gmail.com>
2019-02-20 20:27:45 +0900

    February 20,2019

--- tmp.php
+++ tmp.php
@@ -1,13 +1,4 @@
 <?php
-	print_r(error_get_last());
-	$f=file("base64.txt");
-	$ff="";
-	foreach($f as $a)
-		$ff=$ff.trim($a);
-	$ff=str_split($ff,4);
-	foreach($ff as $f){
-		echo $f." ".strlen($f)."\n";
-		$t=base64_decode($f);
-		echo $t."\n";
-	}
+	$f=file_get_contents("un.txt");
+	echo mb_convert_encoding($f,'UTF-8','SJIS');
 ?>
