49c124f74bd33490ae63eafee52e245d0894dd07
Tortoise100101 <tor.two10101@gmail.com>
2019-02-20 18:14:52 +0900

    February 20,2019  can't decode to utf8 from base64

--- mail.php
+++ mail.php
@@ -1,21 +1,50 @@
 <?php
 	function Extract_spacee0($str){
-		echo $str;
-		$name_s=strpos($str,"お申込みスペース");
-		$price_s=strpos($str,"［合計料金］");
-		$time_s= strpos($str,"［ご利用時間数］");
-		$number_s=strpos($str,"［予約No］");
-		echo "\n\n".$addres_s."\n\n";
-		$space_name=mb_split("\n",substr($str,$name_s))[3];
-		$price=mb_split("\n",substr($str,$price_s))[1];
-		$time= mb_split("\n",substr($str,$time_s))[1];
-		$number=mb_split("\n",substr($str,$number_s))[1];
+		$space_name=strpos($str,"お申込みスペース");$space_name=mb_split("\n",substr($str,$space_name))[3];
+		$price=strpos($str,"［合計料金］");$price=mb_split("\n",substr($str,$price))[1];
+		$time= strpos($str,"［ご利用時間数］");$time= mb_split("\n",substr($str,$time))[1];
+		$number=strpos($str,"［予約No］");$number=mb_split("\n",substr($str,$number))[1];
+		$date=strpos($str,"［ご利用希望日時］");$date=mb_split("\n",substr($str,$date))[1];
+		$client_mail=strpos($str,"［メールアドレス］");$client_mail=mb_split("\n",substr($str,$client_mail))[1];
+		$client_name=strpos($str,"［お名前］");$client_name=mb_split("\n",substr($str,$client_name))[1];
+		$client_phone=strpos($str,"［電話番号］");$client_phone=mb_split("\n",substr($str,$client_phone))[1];
+		$address=strpos($str,"住所:");$address=mb_split(" ",substr($str,$address))[1];
+		$map=strpos($str,"MAP:");$map=mb_split("[ \n]",substr($str,$map))[1];
 		echo $space_name."\n";
 		echo $price."\n";
 		echo $time."\n";
 		echo $number."\n";
+		echo $date."\n";
+		echo $client_mail."\n";
+		echo $client_name."\n";
+		echo $client_phone."\n";
+		echo $address."\n";
+		echo $map."\n";
 	}
-	/*
+	function Extract_spacee1($str){
+		$space_name=strpos($str,"お申込みスペース");$space_name=mb_split("\n",substr($str,$space_name))[3];
+		$price=strpos($str,"［合計料金］");$price=mb_split("\n",substr($str,$price))[1];
+		$time= strpos($str,"［ご利用時間数］");$time= mb_split("\n",substr($str,$time))[1];
+		$number=strpos($str,"［予約No］");$number=mb_split("\n",substr($str,$number))[1];
+		$date=strpos($str,"［ご利用希望日時］");$date=mb_split("\n",substr($str,$date))[1];
+		$client_mail=strpos($str,"［メールアドレス］");$client_mail=mb_split("\n",substr($str,$client_mail))[1];
+		$client_name=strpos($str,"［お名前］");$client_name=mb_split("\n",substr($str,$client_name))[1];
+		$client_phone=strpos($str,"［電話番号］");$client_phone=mb_split("\n",substr($str,$client_phone))[1];
+		$address=strpos($str,"住所:");$address=mb_split(" ",substr($str,$address))[1];
+		$map=strpos($str,"MAP:");$map=mb_split("[ \n]",substr($str,$map))[1];
+		echo $space_name."\n";
+		echo $price."\n";
+		echo $time."\n";
+		echo $number."\n";
+		echo $date."\n";
+		echo $client_mail."\n";
+		echo $client_name."\n";
+		echo $client_phone."\n";
+		echo $address."\n";
+		echo $map."\n";
+		if($space_name=='')
+			echo $str."\n";
+	}
 	$imap='{imap.googlemail.com:993/novalidate-cert/imap/ssl}INBOX';
 	$username='common10111@gmail.com';
 	$password='Tmcitonetwothree';
@@ -24,20 +53,33 @@
 		echo "Failed\n";
 		exit(1);
 	}
-	$message_ids=imap_search($response,'From "no-reply@" BEFORE "10 February 2019"');
+	//$message_ids=imap_search($response,'From "no-reply@" BEFORE "10 February 2019"');
+	$message_ids=imap_search($response,'From "no-reply@"');
 	$file_names=file("title.txt");
 	foreach($message_ids as $message_id){
 		$header=imap_headerinfo($response,$message_id);
 		$subject=mb_decode_mimeheader($header->subject);
-		$body=imap_fetchbody($response,$message_id,1,FT_INTERNAL);
-		$body=base64_decode($body);
-		$body=mb_convert_encoding($body,'UTF-8');
 		if(strpos($subject, trim($file_names[0])) and strpos($subject,"入金を確認しました") ){
-			echo $subject;
+			$body=imap_fetchbody($response,$message_id,1,FT_INTERNAL);
+			$body=base64_decode($body);
+			echo $subject."\n";
 			Extract_spacee0($body);
 		}
+		else if(strpos($subject, trim($file_names[0])) and strpos($subject,"スペース予約確定") ){
+			$body=imap_fetchbody($response,$message_id,1,FT_INTERNAL);
+			if(base64_decode($body)==false){
+				file_put_contents("base64.txt",$body);
+				exit(0);
+			}
+			$body=base64_decode($body);
+			echo $subject."\n";
+			Extract_spacee1($body);
+		}
+		else if(strpos($subject, trim($file_names[0])) and strpos($subject,"未入金の") ){
+			$body=imap_fetchbody($response,$message_id,1,FT_INTERNAL);
+			$body=base64_decode($body);
+			echo $subject."\n";
+			echo $body."\n";
+		}
 	}
-	*/
-	$str=file_get_contents("text.txt");
-	Extract_spacee0($str);
 ?>
--- tmp.php
+++ tmp.php
@@ -1,66 +1,12 @@
 <?php
-	$a=file("titles.txt");
-	$f= "スペイシー";//true
-	$ff="スぺイシー";//false
-	var_dump($f);
-	var_dump($ff);
-	foreach($a as $aa){
-		$t=strpos($aa,$f);
-		$tt=strpos($aa,$ff);
-		if($t){
-			echo '$t ='.$t."\n";
-			echo '$tt='.$tt."\n";
-		}
+	print_r(error_get_last());
+	$f=file_get_contents("base64.txt");
+	$ff=file("base64.txt");
+//	echo mb_convert_encoding($f,'UTF-8','BASE64');
+//	echo base64_decode($f);
+	foreach($ff as $f){
+		echo $f."\n";
+		$t=base64_decode($f);
+		echo $t."\n";
 	}
-/*
-Result:
-
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-$tt=
-$t =1
-*/
 ?>
